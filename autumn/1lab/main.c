8a37d7910624df04fbbaa4fcf7f857f07a8f903c
Pavel Kulik <pashakulik58@gmail.com>
2024-09-17 21:30:24 +0700

    Обучение в процессе

--- autumn/1lab/main.c
+++ autumn/1lab/main.c
@@ -1,18 +1,24 @@
+#include <stdlib.h>
 #include <mpi.h>
 #include <stdio.h>
 
-int main(int argc, char* argv[])
+int main(int argc, char** argv)
 {
-    int commsize, rank, len;
-    char procname[MPI_MAX_PROCESSOR_NAME];
-
+    int rank, commsize;
+    
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &commsize);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Get_processor_name(procname, &len);
-    
-    printf("Hello MPI World! Process %d of %d on node %s.\n", rank, commsize, procname);
 
+    double *buf = malloc(sizeof(*buf) * 100);
+
+    if (rank == 0) {
+        MPI_Send(buf, 100, MPI_DOUBLE, 1, 0, MPI_COMM_WORLD);
+    } else if (rank == 1) {
+        MPI_Recv(buf, 100, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    }
+
+    free(buf);
     MPI_Finalize();
     return 0;
 }
