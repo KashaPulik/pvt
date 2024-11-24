113552b0f22eba1e75eb15e52652087a08ece45e
Kulik Pavel <pashakulik58@gmail.com>
2024-11-24 23:31:55 +0700

    График построен, лаба сделана

--- autumn/4lab/graph.py
+++ autumn/4lab/graph.py
@@ -19,14 +19,14 @@ with open(input_file, 'r') as file:
 initial_time1 = times1[0]
 initial_time2 = times2[0]
 
-speedup1 = [initial_time1 / t * 4 for t in times1]
-speedup2 = [initial_time2 / t * 4 for t in times2]
+speedup1 = [initial_time1 / t * 8 for t in times1]
+speedup2 = [initial_time2 / t * 8 for t in times2]
 
 plt.figure(figsize=(10, 6))
 
-plt.plot(processes, speedup1, marker='o', linestyle='-', color='b', label="28000")
+plt.plot(processes, speedup1, marker='o', linestyle='-', color='b', label="1000")
 
-plt.plot(processes, speedup2, marker='s', linestyle='--', color='r', label="45000")
+plt.plot(processes, speedup2, marker='s', linestyle='--', color='r', label="10000")
 
 plt.plot(range(1,32),range(1,32),'-',c="blue",linewidth=0.5,label="Linear speedup")
 
